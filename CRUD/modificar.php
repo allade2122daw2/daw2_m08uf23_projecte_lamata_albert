b5ad8f0d3d15134829e45e95f39b08d3e27fecd8
Albert Lamata <allade2122daw2@protonmail.com>
2022-05-05 20:11:49 +0200

    CRUD

--- CRUD/modificar.php
+++ CRUD/modificar.php
@@ -1,5 +1,5 @@
 <?php
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 
 use Laminas\Ldap\Attribute;
 use Laminas\Ldap\Ldap;
--- CRUD/visualitzar.php
+++ CRUD/visualitzar.php
@@ -1,5 +1,5 @@
 <?php
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 use Laminas\Ldap\Ldap;
 $domini = 'dc=fjeclot,dc=net';
 $opcions = [
